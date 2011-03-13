6de6a5e3ff287fb32fe79cd9bdf7bf0548730ed1
Lee Gao <leeyuan123@yahoo.com>
2011-03-13 14:48:52 -0700

    Removed stream test from previous commit.

--- src/main.c
+++ src/main.c
@@ -75,35 +75,6 @@ int main(int argc, char *argv[]){
 	init_queue(&send_q);
 	start_stream();
 
-	stream* s = init_stream(NULL_STREAM);
-	Stream.write(s, (byte*)"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"123456789\n"
-			"1234567890\n", 252);
-	//Stream.trim(s, 0);
-	Stream.write(s, "asdfasdf", 9);
-
 	printf("%s %d %d %d\n",s->mem, s -> mem, s->cur, s->size);
 	/**
 	 * \var needed for ExecuteFunctionsWithTime()
